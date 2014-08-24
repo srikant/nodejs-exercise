7a05f870b0d0f945858394907ac9a6afe6505849
srikant <dhondi.srikanth@gmail.com>
2014-08-24 17:04:50 +0530

    Socket.IO Chat application, NodeJS Browserify
    npm init
    
    ngrok -subdomain=srikant 3000
    
    browserify.org
    https://github.com/substack/node-browserify
    
    sudo npm install browserify -g
    browserify browser.js > public/foobar2.js

---
+++ lab11/app.js
@@ -0,0 +1,50 @@
+var express = require('express'), 
+    http = require('http'), 
+    path = require('path');
+
+var app = express();
+
+app.configure(function() {
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function() {
+    app.use(express.errorHandler());
+});
+
+// Set up express
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
+
+// Set up socket.io
+var io = require('socket.io').listen(server);
+
+// Handle socket traffic
+io.sockets.on('connection', function (socket) {
+    
+    // Set the nickname property for a given client
+	// Event listener - nick
+    socket.on('nick', function(nick) {
+        socket.set('nickname', nick);
+    });
+
+    // Relay chat data to all clients
+	// Event listener - chat
+    socket.on('chat', function(data) {
+        socket.get('nickname', function(err, nick) {
+            var nickname = err ? 'Anonymous' : nick;
+
+            var payload = {
+                message: data.message,
+                nick: nickname
+            };
+
+			io.sockets.emit('chat', payload);
+			
+//            socket.emit('chat',payload);
+//            socket.broadcast.emit('chat', payload);
+        });
+    });
+});
